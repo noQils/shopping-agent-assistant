61a8fce9b6e77dcce9559f8107b2dd318d59ec6b
noQils <daffaaqilmahmud@gmail.com>
2026-06-28 22:40:17 +0700

    fix: fix get product rating tool import inside tools init

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,3 +1,4 @@
-from .search_product import search_product, get_product_rating
+from .search_product import search_product
+from .get_product_rating import get_product_rating
 
 tools = [search_product, get_product_rating]
